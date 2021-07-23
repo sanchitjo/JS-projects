0e2e9fa8a81638aaf359d46307d14cc6c5654095
sanchitjo <82511213+sanchitjo@users.noreply.github.com>
2021-07-23 14:49:26 +0530

    Add files via upload

---
+++ setTimeout/newyear.js
@@ -0,0 +1,47 @@
+var year = () => {
+    setTimeout( () => {
+     console.log(10)
+     },1000)
+     
+     setTimeout( () => {
+     console.log(9)
+     },2000)
+   
+     setTimeout( () => {
+     console.log(8)
+     },3000)
+   
+     setTimeout( () => {
+     console.log(7)
+     },4000)
+   
+     setTimeout( () => {
+     console.log(6)
+     },5000)
+   
+     setTimeout( () => {
+     console.log(5)
+     },6000)
+   
+     setTimeout( () => {
+     console.log(4)
+     },7000)
+   
+     setTimeout( () => {
+     console.log(3)
+     },8000)
+   
+     setTimeout( () => {
+     console.log(2)
+     },9000)
+   
+     setTimeout( () => {
+     console.log(1)
+     },10000)
+   
+     setTimeout( () => {
+     console.log("   Happy New Year.    ")
+     },11000)
+   
+   }                                                       
+   year();
